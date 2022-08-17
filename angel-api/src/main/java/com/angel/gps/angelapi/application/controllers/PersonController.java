466ff1ef97ea4bdbc45e0e67e563972d2b5d6d49
monajuan2000 <monajuan2000@gmail.com>
2022-08-17 00:22:19 -0500

    add controller class for login the front end

--- angel-api/src/main/java/com/angel/gps/angelapi/application/controllers/PersonController.java
+++ angel-api/src/main/java/com/angel/gps/angelapi/application/controllers/PersonController.java
@@ -1,4 +1,21 @@
 package com.angel.gps.angelapi.application.controllers;
 
+import com.angel.gps.angelapi.core.models.PersonModel;
+import com.angel.gps.angelapi.core.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/angel-gps/persons")
 public class PersonController {
+
+   @Autowired
+   private PersonService personService;
+
+   @GetMapping(value = "/login/{username}")
+   public Mono<PersonModel> findByUsername(@PathVariable String username){
+      return personService.findByUsername(username);
+   }
 }
