fb05c5de4f2d598ebded16caa76db37bc66c3722
monajuan2000 <monajuan2000@gmail.com>
2023-05-12 16:42:01 -0500

    cheanges in the proyect api

--- angel-api/src/main/java/com/angel/gps/angelapi/core/services/PersonService.java
+++
@@ -1,9 +0,0 @@
-package com.angel.gps.angelapi.core.services;
-
-import com.angel.gps.angelapi.core.models.PersonModel;
-import reactor.core.publisher.Mono;
-
-public interface PersonService {
-
-   Mono<PersonModel> findByUsername(String username);
-}
--- angel-api/src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO role (name) VALUES ('ADMIN');
-INSERT INTO role (name) VALUES ('USER');
-
-INSERT INTO person (name,email,username,password) VALUES ('Juan Esteban','monajuan2000@gmail.com','monajuan2000','juanes123456');
-INSERT INTO person (name,email,username,password) VALUES ('Angie Parra','angie@gmail.com','angie2000','angie123456');
